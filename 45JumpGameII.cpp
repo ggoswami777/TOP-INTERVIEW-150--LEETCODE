3d5410a119d149f65b602ab9ce8d9813cfefb001
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-04-26 21:22:22 +0530

    45. Jump Game II

---
+++ 45JumpGameII.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        int l=0,r=0,jumps=0;
+        while(r<n-1){
+            int far=0;
+            for(int i=l;i<=r;i++){
+                far=max(far,nums[i]+i);
+            }
+            l=r+1;
+            r=far;
+            jumps++;
+        }
+        return jumps;
+    }
+};
