9d1c434d5844a7657d45a19e9c18549aeb632ff6
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-05-02 10:41:29 +0530

    135. Candy

---
+++ 135Candy.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        int sum=1;
+        int i=1;
+        while(i<n){
+
+            // for flat surfaces
+            while(i<n && ratings[i]==ratings[i-1]){
+                sum++;
+                i++;
+            }
+             
+            // for increasing surfaces
+            int peak=1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak++;
+                sum+=peak;
+                i++;
+            }
+
+            // for decreasing surfaces
+            int down=1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                sum+=down;
+                down++;
+                i++;
+            }
+            if(down>peak){
+                sum+=down-peak;
+            }
+        
+        }
+        return sum;
+    }
+};
